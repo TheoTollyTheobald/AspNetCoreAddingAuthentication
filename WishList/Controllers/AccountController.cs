66a7ba4295bb9934ac1c887df73536c9a6a5b544
TheoTollyTheobald <Theo.Theobald@nottscc.gov.uk>
2022-07-04 15:44:19 +0100

    Section 3 - part 1

--- WishList/Controllers/AccountController.cs
+++ WishList/Controllers/AccountController.cs
@@ -2,13 +2,37 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using WishList.Models;
+using WishList.Models.AccountViewModels;
+using WishList.Controllers;
 
 namespace WishList.Controllers
 {
-    [Authorize]public class AccountController : Controller
+    [Authorize] 
+    public class AccountController : Controller
     {
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly SignInManager<ApplicationUser> _signInManager;
+
+        [HttpGet]
+        [AllowAnonymous]
+        private IActionResult Register()
+        {
+            return View("Register");
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        private IActionResult Register(RegisterViewModel registerViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("HomeController.Index");
+            }
+            else
+            {
+                return View(registerViewModel);
+            }  
+        }
 
         public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
         {
---
+++ WishList/Models/AccountViewModels/RegisterViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WishList.Models.AccountViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required][EmailAddress] string Email
+        {
+            get; set;
+        }
+        [Required][StringLength(100)][MinLength(8)][DataType(DataType.Password)] string Password
+        {
+            get; set;
+        }
+        [Required][DataType(DataType.Password)][Compare("Password")] string ConfirmPassword
+        {
+            get; set;
+        }
+    }
+}
