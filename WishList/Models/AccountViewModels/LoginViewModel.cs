b1b295b513e090c888aaf1d18cf8338e053311b5
TheoTollyTheobald <Theo.Theobald@nottscc.gov.uk>
2022-07-05 11:19:57 +0100

    Section 4

---
+++ WishList/Models/AccountViewModels/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WishList.Models.AccountViewModels
+
+{
+    public class LoginViewModel
+    {
+        [EmailAddress]
+        [Required]
+        public string Email { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required]
+        public string Password { get; set; }
+    }
+}
